e8e4ded444702d13b141860bf7110947ada43665
Felipe Vasconcelos <felipe.conexaodev@gmail.com>
2025-05-29 22:14:20 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+const qrCodeImg = document.querySelector("#qr-code img");
+// Eventos
+
+// Gerar QR Code
+function generateQRCode() {
+  const qrCodeInputValue = qrCodeInput.value;
+  if (!qrCodeInputValue) return;
+
+  qrCodeBtn.innerText = "Gerando QR Code...";
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerText = "QR Code gerado!";
+  });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQRCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    generateQRCode();
+  }
+});
+
+// Limpar área do QR Code
+qrCodeInput.addEventListener("keyup", () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove("active");
+    qrCodeBtn.innerText = "Gerar QR Code";
+  }
+});
